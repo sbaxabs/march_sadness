38dca657113421a8c841f63f1f7429e5efabf0dd
sbaxabs <shirazbaxamusa@gmail.com>
2024-03-09 19:05:56 -0600

    something

--- raw_data_scraper.py
+++ raw_data_scraper.py
@@ -1,5 +1,5 @@
 import pandas
-bracket = ["","sock"]
+bracket = ["Alabama Crimson Tide","Texas A&M-Corpus Christi Islanders","Maryland Terrapins","West Virginia Mountaineers","San Diego State Aztecs","Charleston Cougars","Virginia Cavaliers","Furman Paladins","Creighton Bluejays","NC State Wolfpack","Baylor Bears","UC Santa Barbara Gauchos","Missouri Tigers","Utah State Aggies","Arizona Wildcats","Princeton Tigers","Houston Cougars","Northern Kentucky Norse","Iowa Hawkeyes","Auburn Tigers","Miami Hurricanes","Drake Bulldogs","Indiana Hoosiers","Kentucky Wildcats","Iowa State Cyclones","Pittsburgh Panthers","Xavier Musketeers","Kennesaw State Owls","Texas A&M Aggies","Penn State Nittany Lions","Texas Longhorns","Colgate Raiders","Purdue Boilermakers","Fairleigh Dickinson Knights","Memphis Tigers","Florida Atlantic Owls","Duke Blue Devils","Oral Roberts Golden Eagles","Tennessee Volunteers","Louisiana Ragin' Cajuns","Kentucky Wildcats","Providence Friars","Kansas State Wildcats","Montana State Bobcats","Michigan State Spartans","USC Trojans","Marquette Golden Eagles","Vermont Catamounts","Kansas Jayhawks","Howard Bison","Arkansas Razorbacks","Illinois Fighting Illini","Saint Mary's Gaels","VCU Rams","UConn Huskies","Iona Gaels","TCU Horned Frogs","Arizona State Sun Devils","Gonzaga Bulldogs","Grand Canyon Lopes","Northwestern Wildcats","Boise State Broncos","UCLA Bruins","UNC Asheville Bulldogs"]
 print(bracket)
 df = pandas.read_excel("team_data.xlsx")
 def EffFGP(row):
@@ -17,4 +17,5 @@ df["Rebound Rate"] = df.apply(RebRate, axis = 1)
 df["Turnover Rate"] = df.apply(TurnoverRate, axis = 1)
 df["Free Throw Rate"] = df.apply(FreeThrowRate, axis = 1)
 print(df)
+
 
