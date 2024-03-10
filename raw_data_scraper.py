603081c8216b0547abcb6ff9eabd06ac8488f29b
sbaxabs <shirazbaxamusa@gmail.com>
2024-03-09 18:31:27 -0600

    init formulas for calcs from pi-rate

--- raw_data_scraper.py
+++ raw_data_scraper.py
@@ -2,3 +2,19 @@ import pandas
 bracket = ["","sock"]
 print(bracket)
 df = pandas.read_excel("team_data.xlsx")
+def EffFGP(row):
+    return((row["FGM"]+(.5*row["3PM"]))/row["FGA"])
+def RebRate(row):
+    return((row["OR"]/(row["OR"]+row["DR"])))
+def TurnoverRate(row):
+    possesions = (row["FGA"]+(.475*row["FTA"])-row["OR"]+row["TO"])/row["GP"]
+    return(row["TO"]/possesions)
+def FreeThrowRate(row):
+    return(row["FTA"]/row["FGA"])
+
+df["Effective Field Goal %"] = df.apply(EffFGP, axis = 1)
+df["Rebound Rate"] = df.apply(RebRate, axis = 1)
+df["Turnover Rate"] = df.apply(TurnoverRate, axis = 1)
+df["Free Throw Rate"] = df.apply(FreeThrowRate, axis = 1)
+print(df)
+
